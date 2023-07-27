d84c5d6dc7f147f167d410ddfd07bb7f9b1de60a
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2023-07-27 16:40:45 -0700

    Finished exercise 3.2

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (request, response) => {
     response.json(entries);
 })
 
+app.get('/info', (request, response) => {
+    response.send(`
+    <p>Phonebook has info for ${entries.length} people</p>
+    <p>${new Date()}</p>`)
+})
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
