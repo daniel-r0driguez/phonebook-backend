7cbc97c9ee74fd962de3eeb79f5513ae744e58c8
Daniel Rodriguez <daniel.rodriguez6421@gmail.com>
2023-08-09 18:29:52 -0700

    Added frontend buildversion to phonebook-backend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 let entries = [
@@ -26,7 +27,8 @@ let entries = [
 ]
 
 app.use(express.json());
-
+app.use(cors());
+app.use(express.static('build'));
 app.use(morgan((tokens, req, res) => {
     console.log(req.body);
         return [
@@ -109,7 +111,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
